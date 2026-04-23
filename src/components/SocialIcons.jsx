fc114136486f7411c6f0c42f99512f47222efdaa
thatengineerguy21 <91711919+thatengineerguy21@users.noreply.github.com>
2026-04-23 19:42:59 +0530

    feat(socials): added medium blogs social link

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -5,11 +5,13 @@ import githubIcon from '../assets/icons/GitHub.svg';
 import linkedinIcon from '../assets/icons/linkedin.svg';
 import twitterIcon from '../assets/icons/x.svg';
 import instagramIcon from '../assets/icons/instagram.svg';
+import mediumIcon from '../assets/icons/medium.svg';
 
 const socials = [
   { name: 'GitHub', link: 'https://github.com/thatengineerguy21', icon: githubIcon, hoverColor: { surface: 'rgba(108, 198, 68, 0.12)', border: 'rgba(108, 198, 68, 0.30)' } },
   { name: 'LinkedIn', link: 'https://linkedin.com/in/thatengineerguy13', icon: linkedinIcon, hoverColor: { surface: 'rgba(10, 102, 194, 0.12)', border: 'rgba(40, 130, 220, 0.30)' } },
   { name: 'Twitter', link: 'https://twitter.com/thisengineerguy', icon: twitterIcon, hoverColor: { surface: 'rgba(200, 200, 200, 0.10)', border: 'rgba(255, 255, 255, 0.25)' } },
+  { name: 'Medium', link: 'https://medium.com/@thatengineerguy', icon: mediumIcon, hoverColor: { surface: 'rgba(108, 198, 68, 0.12)', border: 'rgba(21, 109, 18, 0.25)' } },
   { name: 'Instagram', link: 'https://instagram.com/thatengineerguy13', icon: instagramIcon, hoverColor: { surface: 'rgba(231, 68, 86, 0.5)', border: 'rgba(224, 38, 60, 0.85)' } },
 ];
 
